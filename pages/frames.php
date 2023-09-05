4e9bcf734e6fb476e49215af7915c0cd927306a9
unknown <ngangmojordan@gmail.com>
2023-09-05 15:51:52 +0100

    update de la description

---
+++ pages/frames.php
@@ -0,0 +1,11 @@
+<FRAMESET cols="99.90%,0.1%" frameborder= yes bordercolor=#FFF000>
+	        
+        
+
+<FRAME SRC="../signUp.php" NAME="haut" scrolling= auto>
+<FRAME SRC="../JavaScript-Password-Generator/index.html" NAME="bas" scrolling= auto >
+
+
+
+
+
